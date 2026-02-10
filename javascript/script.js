887b44f7ab91544592a7f73e1e25b305854f5272
Marcos <marquinhosmvx@gmail.com>
2026-02-09 22:45:34 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,32 @@ function entrar() {
   window.location.href = "menu.html";
 }
 
+function entrar() {
+  criarBorboletas();
+
+  setTimeout(() => {
+    window.location.href = "menu.html";
+  }, 1200);
+}
+
+function criarBorboletas() {
+  for (let i = 0; i < 15; i++) {
+    let b = document.createElement("img");
+    b.src = "img/borboleta.png";
+    b.className = "butterfly";
+
+    b.style.left = "50%";
+    b.style.top = "50%";
+
+    let x = (Math.random() - 0.5) * 800 + "px";
+    let y = (Math.random() - 0.5) * 800 + "px";
+
+    b.style.setProperty("--x", x);
+    b.style.setProperty("--y", y);
+
+    document.body.appendChild(b);
+
+    setTimeout(() => b.remove(), 1500);
+  }
+}
+
