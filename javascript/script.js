e3f26ff03f82d4d23f1d61a41e62adad38205f94
Marcos <marquinhosmvx@gmail.com>
2026-02-09 23:03:21 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,17 @@
-function entrar() {
-  window.location.href = "menu.html";
-}
+window.onload = () => {
+  criarBorboletasFundo();
+};
 
 function entrar() {
-  criarBorboletas();
+  explosaoBorboletas();
 
   setTimeout(() => {
     window.location.href = "menu.html";
-  }, 1200);
+  }, 1500);
 }
 
-function criarBorboletas() {
+/* Explosão ao clicar */
+function explosaoBorboletas() {
   for (let i = 0; i < 15; i++) {
     let b = document.createElement("img");
     b.src = "img/borboleta.png";
@@ -18,31 +19,40 @@ function criarBorboletas() {
 
     b.style.left = "50%";
     b.style.top = "50%";
-
-    let x = (Math.random() - 0.5) * 800 + "px";
-    let y = (Math.random() - 0.5) * 800 + "px";
-
-    b.style.setProperty("--x", x);
-    b.style.setProperty("--y", y);
+    b.style.opacity = "1";
 
     document.body.appendChild(b);
+
+    setTimeout(() => {
+      let x = (Math.random() - 0.5) * window.innerWidth;
+      let y = (Math.random() - 0.5) * window.innerHeight;
+
+      b.style.transform = `translate(${x}px, ${y}px)`;
+      b.style.opacity = "0";
+    }, 50);
 
     setTimeout(() => b.remove(), 1500);
   }
 }
 
-function borboletasFundo() {
+/* Borboletas subindo no fundo */
+function criarBorboletasFundo() {
   setInterval(() => {
     let b = document.createElement("img");
     b.src = "img/borboleta.png";
-    b.className = "butterfly-bg";
+    b.className = "butterfly";
 
-    b.style.left = Math.random() * 100 + "vw";
+    b.style.left = Math.random() * window.innerWidth + "px";
+    b.style.top = window.innerHeight + "px";
+    b.style.opacity = "0.7";
 
     document.body.appendChild(b);
 
-    setTimeout(() => b.remove(), 20000);
+    setTimeout(() => {
+      b.style.transform = `translateY(-${window.innerHeight + 200}px)`;
+      b.style.opacity = "0";
+    }, 50);
+
+    setTimeout(() => b.remove(), 15000);
   }, 3000);
 }
-
-borboletasFundo();
