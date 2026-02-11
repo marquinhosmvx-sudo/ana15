e98be7d9ca5400a7128be39668a74f7ac8d410c2
Marcos <marquinhosmvx@gmail.com>
2026-02-10 22:24:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -108,3 +108,26 @@ function navegar(pagina) {
     window.location.href = pagina;
   }, 500);
 }
+function atualizarContador() {
+
+  let dataEvento = new Date("2026-05-16T19:19:00");
+
+  let agora = new Date();
+  let diferenca = dataEvento - agora;
+
+  if (diferenca <= 0) {
+    document.getElementById("contador").innerText =
+      "Hoje é o grande dia!";
+    return;
+  }
+
+  let dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+  let horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+  let minutos = Math.floor((diferenca / 1000 / 60) % 60);
+
+  document.getElementById("contador").innerText =
+    `Faltam ${dias} dias, ${horas}h e ${minutos}min`;
+}
+
+setInterval(atualizarContador, 60000);
+atualizarContador();
